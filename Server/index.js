e8022c44ffc0fbde0d398349b4288f0c202ab587
Sanu334 <maadarabuu132@gmail.com>
2024-03-24 19:45:59 +0530

    custom env variable added, personal Mongo db url removed

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 const config = require("config");
+
 if (!config.get("privateKey")) {
   console.error("FATAL ERROR: privateKey is not defined");
   process.exit(1);
@@ -8,11 +9,7 @@ if (!config.get("privateKey")) {
 
 //connect data then start server
 const port = process.env.PORT || 3030;
-mongoose
-  .connect(
-    "mongodb+srv://test:1322001Maajin@cluster0.ione1sb.mongodb.net/RealEstate?retryWrites=true&w=majority&appName=Cluster0"
-  )
-  .then(() => {
-    console.log("connected to data base...now ");
-    app.listen(port, () => console.log("listening on.. port", port));
-  });
+mongoose.connect(config.get("MONGOURL")).then(() => {
+  console.log("connected to data base... ");
+  app.listen(port, () => console.log("listening on port...", port));
+});
